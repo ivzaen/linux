060759017811558b123ea30cb9360ebcd52f7aac
Ivan Zaentsev <ivan.zaentsev@wirenboard.ru>
2020-09-25 12:35:13 +0300

    TODO: rebase. Скрипт для тестирования сборки ball.sh, defconf.sh

--- defconf.sh
+++ defconf.sh
@@ -1,1 +1,10 @@
-make -j8 ARCH=arm LOCALVERSION=ivz imx6_wirenboard_defconfig
+#!/bin/bash
+
+yell() { echo "$0: $*" >&2; }
+die() { echo -e "\e[31m$*\e[0m"; exit 1; }
+try() { "$@" || die "cannot $*"; }
+set -x
+
+if [ $# ]; then KERNEL_DEFCONFIG=$1; else KERNEL_DEFCONFIG=imx6_wirenboard_defconfig; fi
+
+try make -j8 ARCH=arm LOCALVERSION=ivz $KERNEL_DEFCONFIG
