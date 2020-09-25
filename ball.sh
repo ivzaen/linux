c8767323fe84525d78ba08759bbb71ccaabfd255
Ivan Zaentsev <ivan.zaentsev@wirenboard.ru>
2020-09-25 12:35:13 +0300

    TODO: rebase. Скрипт для тестирования сборки ball.sh, defconf.sh

---
+++ ball.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export CROSS_COMPILE=arm-linux-gnueabihf-
+
+yell() { echo "$0: $*" >&2; }
+die() { echo -e "\e[31m$*\e[0m"; exit 1; }
+try() { "$@" || die "cannot $*"; }
+set -x
+
+for KERNEL_DEFCONFIG in imx6_wirenboard_defconfig mxs_wirenboard_defconfig; do 
+  [ "$KERNEL_DEFCONFIG" = "mxs_wirenboard_defconfig" ] && DEBARCH=armel
+  [ "$KERNEL_DEFCONFIG" = "imx6_wirenboard_defconfig" ] && DEBARCH=armhf
+  echo $KERNEL_DEFCONFIG $DEBARCH
+  try make -j8 ARCH=arm LOCALVERSION=ivz $KERNEL_DEFCONFIG
+  try time make -j8 ARCH=arm KBUILD_DEBARCH=$DEBARCH LOCALVERSION=ivz zImage modules dtbs
+done
+
+
